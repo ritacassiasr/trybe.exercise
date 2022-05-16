3937505550ff57118b88d2cdc498def097e880fa
Rita Rocha <ritacassiasr@gmail.com>
2022-05-16 18:31:41 -0400

    correção exercício 8 e elaboração exercício 9

--- dia-1-javascript-primeiros-passos/exercício_8.js
+++ dia-1-javascript-primeiros-passos/exercício_8.js
@@ -7,6 +7,6 @@ const x = 5
 const y = 3
 const z = 9
 
-if (x % 2 == 0 || y % 2 == 0 || z == 0) {
+if (x % 2 == 0 || y % 2 == 0 || z % 2 == 0) {
     console.log("true")
 } else console.log("false")
