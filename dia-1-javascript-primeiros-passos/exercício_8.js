66e8097c0e3c35bd76e4f01ff1f52e5a356139ad
Rita Rocha <ritacassiasr@gmail.com>
2022-05-16 18:23:56 -0400

    exercício 8

---
+++ dia-1-javascript-primeiros-passos/exercício_8.js
@@ -0,0 +1,12 @@
+// 🚀 Escreva um programa que defina três números em constantes e
+// retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+
+const x = 5
+const y = 3
+const z = 9
+
+if (x % 2 == 0 || y % 2 == 0 || z == 0) {
+    console.log("true")
+} else console.log("false")
