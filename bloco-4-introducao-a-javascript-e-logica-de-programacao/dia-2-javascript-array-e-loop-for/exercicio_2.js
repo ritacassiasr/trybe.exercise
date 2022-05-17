88bfc7b264a64aad676da19c0a75d8601da79051
Rita Rocha <ritacassiasr@gmail.com>
2022-05-17 17:22:01 -0400

    alteração exercicio 1 e 2

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_2.js
@@ -0,0 +1,11 @@
+// 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var soma = 
+
+for (index = 0; index < numbers.length; index +-1) {
+    console.log("soma dos valores é igual a "(soma
+        
+        
+        (index))
+}
