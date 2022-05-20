659a45d46585ba72bddf4d2735c7122685bff466
Rita Rocha <ritacassiasr@gmail.com>
2022-05-20 17:34:42 -0400

    alteração nome arquivos html e js

---
+++ bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script_parte_1.js
@@ -0,0 +1,6 @@
+// 1. Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito
+document.getElementById("page-title").innerText = "Euphoria"
+// 2. Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+document.querySelector('#second-paragraph').innerText = "É uma série da HBO, estrelada pela atriz 'Zendaya'."
+// 3. Por fim, recupere o subtítulo e altere-o também
+document.querySelector('#subtitle').innerText = "Série aclamada"
