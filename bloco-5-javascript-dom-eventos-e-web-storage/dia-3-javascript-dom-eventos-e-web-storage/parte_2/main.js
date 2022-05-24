4b984d89e02566a4cfad0166de7ee5e7a6bc69ac
Rita Rocha <ritacassiasr@gmail.com>
2022-05-24 18:06:47 -0400

    exercícios dom bloco 5

--- bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-dom-eventos-e-web-storage/parte_2/main.js
+++ bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-dom-eventos-e-web-storage/parte_2/main.js
@@ -21,8 +21,12 @@ const elementTech = document.querySelector('.tech');
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
+function redirect (event)
+
+
 // 4.1. Que tal redirecionar para seu portfólio?
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
