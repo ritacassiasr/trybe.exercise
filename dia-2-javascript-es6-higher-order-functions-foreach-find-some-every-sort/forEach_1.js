3b8499882bf0b9ff97f75626171bb32f7bf90d05
Rita Rocha <ritacassiasr@gmail.com>
2022-07-02 18:54:06 -0400

    exercicio forEach_1

---
+++ dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/forEach_1.js
@@ -0,0 +1,45 @@
+const students = [
+    { name: 'Maria', grade: 70, approved: '' },
+    { name: 'José', grade: 56, approved: '' },
+    { name: 'Roberto', grade: 90, approved: '' },
+    { name: 'Ana', grade: 81, approved: '' },
+  ];
+
+
+//usando o forEach
+function studentsResult() {
+    students.forEach((student) => {
+    if (student.grade >= 60) {
+        student.approved = 'Aprovado' 
+    } else {
+        student.approved = 'Reprovado'
+     }
+    })
+}
+studentsResult();
+
+
+
+  // -----------> <------------------------
+  // usando o for tradicional 
+
+  /* function verifyGrades() {
+    for (let index = 0; index < students.length; index += 1) {
+      const student = students[index];
+      if (student.grade >= 60) {
+        student.approved = 'Aprovado';
+      } else {
+        student.approved = 'Recuperação';
+      }
+    }
+  }
+  
+  verifyGrades();
+  
+  console.log(students); */
+  // [
+  //   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+  //   { name: 'José', grade: 56, approved: 'Recuperação' },
+  //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+  //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+  // ]
