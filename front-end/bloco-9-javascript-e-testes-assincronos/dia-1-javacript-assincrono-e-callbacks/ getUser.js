8fc69d5c3421cab19695ecf01370bfe964a66e35
Rita Rocha <ritacassiasr@gmail.com>
2022-07-13 16:38:53 -0400

    exercicios bloco 9

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javacript-assincrono-e-callbacks/ getUser.js
@@ -0,0 +1,16 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (xablau) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  
+  return xablau(userToReturn);
+  
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
